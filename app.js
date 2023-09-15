f33b47d77edf30e28fbf186ac71efea254f7c30c
nitesh <niteshregmi03@gmail.com>
2023-09-15 16:22:43 +0530

    lol

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const path = require('path');
+const open = require('open');  // Add this line to include the open module
 
 const app = express();
 
-// to support JSON-encoded bodies
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-  // Your webhook secret from Razorpay Dashboard
   const secret = 'padhae';
-
-  // The Razorpay signature in the request header
   const signature = req.headers['x-razorpay-signature'];
-
-  // Generate the expected signature based on the request body
   const generatedSignature = crypto
     .createHmac('sha256', secret)
     .update(JSON.stringify(req.body))
     .digest('hex');
 
-  // Check if the generated signature matches the request signature
   if (generatedSignature === signature) {
-    // Extract payment ID
     const paymentId = req.body.payload.payment.entity.id;
-
-    // Log the Payment ID
     console.log('Payment was successful. Payment ID:', paymentId);
-
-    // TODO: Further processing, like updating database records.
-
     res.status(200).send('OK');
   } else {
     res.status(400).send('Invalid signature');
@@ -38,4 +29,5 @@ app.post('/webhook', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
+  open('http://localhost:3000');  // Add this line to automatically open the web browser
 });
